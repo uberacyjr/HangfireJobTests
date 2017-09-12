1a91e4278c83d0a9c6ca8305b36862a922df8e1c
Uberacy junior <uberacyjr@gmail.com>
2017-09-11 23:37:06 -0300

    add web project

--- Hangfire.Test/Hangfire.Test/Program.cs
+++ Hangfire.Test/Hangfire.Test/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Hangfire");
+
+            using (var server = new BackgroundJobServer())
+            {
+                System.Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                System.Console.ReadKey();
+            }
         }
+        
     }
 }
